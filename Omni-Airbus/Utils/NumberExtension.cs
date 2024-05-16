99152e1525eee769262c28e84893844384b776b2
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2024-05-16 18:31:19 +0200

    Utils ideas

--- Omni-Airbus/Utils/NumberExtension.cs
+++ Omni-Airbus/Utils/NumberExtension.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Omni_Airbus.Utils
 {
-    internal class NumberExtension
+    public static class NumberExtension
     {
+        /// <summary>
+        /// Converts the represented int value to milliseconds
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ToMilliseconds(this int value)
+        {
+            return value * 1000;
+        }
+
+        /// <summary>
+        /// Converts the represented float value to milliseconds
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ToMilliseconds(this float value)
+        {
+            return Convert.ToInt32(value * 1000f);
+        }
+
+        /// <summary>
+        /// Converts the represented int value to minutes
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ToMinutes(this int value)
+        {
+            return value / 60;
+        }
     }
 }
