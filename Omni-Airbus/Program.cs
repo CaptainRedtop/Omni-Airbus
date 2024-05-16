186486a79d6c8821e82289dbd66e540d4b6061ad
CaptainRedtop <klich.niklas@hotmail.com>
2024-05-16 12:29:08 +0200

    Add project files.

---
+++ Omni-Airbus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Omni_Airbus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
