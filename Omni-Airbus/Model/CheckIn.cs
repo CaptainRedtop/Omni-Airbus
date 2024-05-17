b30449621ac0deed97fd839594d1f042f48a259a
Flustivs <rune170206@gmail.com>
2024-05-17 12:31:39 +0200

    Filled in Terminal and checkin and the beginning of luggagesystem

--- Omni-Airbus/Model/CheckIn.cs
+++ Omni-Airbus/Model/CheckIn.cs
@@ -9,6 +9,20 @@ namespace Omni_Airbus.Model
     internal class CheckIn
     {
         public bool Registered;
+        private int ID = 0;
+        private ConveyerBelt InboundBelt;
+
+        public CheckIn(ConveyerBelt inboundbelt)
+        {
+            InboundBelt = inboundbelt;
+        }
+
+        public void Checked(Luggage luggage)
+        {
+            ID++;
+            luggage.baggageID = ID;
+            InboundBelt.Enqueue(luggage);
+        }
 
     }
 }
--- Omni-Airbus/Model/Terminal.cs
+++ Omni-Airbus/Model/Terminal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Omni_Airbus.Utils.Logger;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,32 @@ using System.Threading.Tasks;
 
 namespace Omni_Airbus.Model
 {
-    public class Terminal
+    public class Terminal : Consumer
     {
+        public ConveyerBelt InboundBelt;
+        public Luggage CurrentLuggage;
+        private Logger log;
         public bool Gate;
+        private readonly object obj = new object();
+
+        public Terminal(ConveyerBelt inboundbelt) : base(inboundbelt)
+        {
+            InboundBelt = inboundbelt;
+            log = new Logger(0);
+        }
+
+        public void Pull(object obj)
+        {
+            while (true)
+            {
+                lock (obj)
+                {
+                    CurrentLuggage = InboundBelt.Dequeue();
+                    log.Information($"{CurrentLuggage.baggageID}");
+                }
+            }
+
+        }
 
     }
 }
