87137e676abca9729543c4973e2d207180caa67e
LucasFair <141710445+LucasFair@users.noreply.github.com>
2024-05-17 09:59:40 +0200

    Fixed access modifiers, pushed into Rune. -Lucas 2024-05-17, 09:59.

--- Omni-Airbus/Model/Booking/AvailableSeat.cs
+++ Omni-Airbus/Model/Booking/AvailableSeat.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Omni_Airbus.Model.Booking
+﻿namespace Omni_Airbus.Model.Booking
 {
 	/// <summary>
 	/// The available seats for a flight with a flight ID and available seats.
 	/// </summary>
-	internal class AvailableSeat
+	public class AvailableSeat
 	{
 		private int _flightId;
 		private int _availableSeats;
