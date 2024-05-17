b0060165d4b4e4de1c287585c91ba41804e61881
CaptainRedtop <klich.niklas@hotmail.com>
2024-05-17 10:08:46 +0200

    IPusher

--- Omni-Airbus/Model/IPusher.cs
+++ Omni-Airbus/Model/IPusher.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Omni_Airbus.Model
 {
+    /// <summary>
+    /// Defines the functionality for an object that can push luggage to a conveyorbelt
+    /// </summary>
     internal interface IPusher
     {
+        /// <summary>
+        /// A blueprint for the Push method
+        /// </summary>
+        /// <param name="obj"></param>
+        void Push(object obj);
     }
 }
